d5a8f66a4cde12a92043586b8fa89b7149e9799b
Karter <kdlmuzixnerd14@gmail.com>
2022-07-07 12:24:54 -0700

    commit tree a0381a3dad2470810aff3a036240e2a747613579

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-// write your code here
+const url = "http://localhost:3000/ramens";
+
+document.addEventListener("DOMContentLoaded", getRamen)
+
+function getRamen()
+{
+    fetch(url)
+    .then(res => res.json())
+    .then(res => renderRamenPics(res))
+
+    const form = document.getElementById('new-ramen');
+    
+    form.addEventListener('submit', (e) => 
+    {
+        e.preventDefault();
+
+        const ramenObj = {};
+
+        ramenObj.name = document.getElementById('new-name').value;
+        ramenObj.restaurant = document.getElementById('new-restaurant').value;
+        ramenObj.image = document.getElementById('new-image').value;
+        ramenObj.rating = document.getElementById('new-rating').value;
+        ramenObj.comment = document.getElementById('new-comment').value;
+
+        displayRamen(ramenObj);
+    })
+    
+}
+
+function displayRamen(item)
+{
+    const image = document.createElement('img')
+    image.src = item.image;
+    
+    document.querySelector("#ramen-menu").appendChild(image);
+    
+    image.addEventListener('click', () => 
+    {
+        document.querySelector(".detail-image").src = item.image;
+        document.querySelector(".name").textContent = item.name;
+        document.querySelector(".restaurant").textContent = item.restaurant;
+        document.querySelector("#rating-display").textContent = item.rating;
+        document.querySelector("#comment-display").textContent = item.comment;
+    })
+}
+
+function renderRamenPics(ramen)
+{
+    ramen.forEach(item => 
+    {
+       displayRamen(item);
+    });   
+}
+
+
+
+
+
